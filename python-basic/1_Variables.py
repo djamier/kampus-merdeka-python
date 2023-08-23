c931352e17cddd7c82d9166f48ac97397842623b
sastriodjamier <sastriodjamier@gmail.com>
2023-08-24 03:19:14 +0700

    learn variables

---
+++ python-basic/1_Variables.py
@@ -0,0 +1,21 @@
+first_name = 'sastrio'
+middle_name = 'yunanda'
+last_name = 'djamier'
+
+print (first_name)
+print (middle_name)
+print (last_name)
+# print (first_name, middle_name, last_name)
+
+# one value to multiple variable
+x = y = z = 'sastrio'
+
+print (x, y ,z)
+
+# many value to multiple variable
+x, y, z = 'sastrio', 'yunanda', 'djamier'
+
+print(x)
+print(y)
+print(z)
+# print (x, y, z)
